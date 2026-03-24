d44dbf6627e01928ec44d26d303370f457222d51
kirsikakutsen <kirsika.kytsen@gmail.com>
2026-03-24 15:25:54 +0100

    fix: add ColorScheme field to DTO

--- internal/dto/request/auth.go
+++ internal/dto/request/auth.go
@@ -3,4 +3,5 @@ package request
 type AuthRequestDto struct {
 	Username string `json:"username" validate:"required"`
 	Password string `json:"password" validate:"required"`
+	ColorScheme string `json:"color_scheme"`
 }
